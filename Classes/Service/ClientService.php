92dbefe2261bc935298290bd444d0a11cd4d9b21
Stefan Lamm <s.lamm@andersundsehr.com>
2022-01-19 14:45:29 +0100

    [FEATURE] provide possibility to let the stack know about the host we want to work with

--- Classes/Service/ClientService.php
+++ Classes/Service/ClientService.php
@@ -67,6 +67,7 @@ class ClientService extends AbstractService
             ],
             'headers' => [
                 'User-Agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:54.0) Gecko/20100101 Firefox/54.0',
+                'Http-Host' => $domain
             ],
         ];
 
